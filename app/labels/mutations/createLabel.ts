bc5aaf75f61493c956e0dc4f4767542164da377e
lucasbogerd <lucas@youngones.works>
2021-05-12 16:40:30 +0200

    style(app): fix whitespacing

--- app/labels/mutations/createLabel.ts
+++ app/labels/mutations/createLabel.ts
@@ -3,8 +3,6 @@ import db from "db"
 import { CreateLabel } from "../validations"
 
 export default resolver.pipe(resolver.zod(CreateLabel), resolver.authorize(), async (input) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
   const label = await db.label.create({ data: input })
-
   return label
 })
