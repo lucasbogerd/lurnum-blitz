fc81f476bf74d29329413d2253e7026e29df74f7
lucasbogerd <lucas@youngones.works>
2021-05-12 16:25:58 +0200

    feat(crud): add Label pages

---
+++ app/labels/pages/add.tsx
@@ -0,0 +1,19 @@
+import Layout from "app/core/layouts/Layout"
+import { BlitzPage } from "blitz"
+import { CreateLabelForm } from "../components/CreateLabelForm"
+
+const AddLabelPage: BlitzPage = () => {
+  return (
+    <div>
+      <CreateLabelForm
+        onSuccess={() => {
+          console.log("lekker gewerkt pik")
+        }}
+      />
+    </div>
+  )
+}
+
+AddLabelPage.getLayout = (page) => <Layout title="Voeg label toe">{page}</Layout>
+
+export default AddLabelPage
