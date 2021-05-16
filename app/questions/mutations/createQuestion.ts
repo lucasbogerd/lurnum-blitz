78d118259db03b1fdf0eecca5d1bc93e8f28a958
lucasbogerd <lucas@youngones.works>
2021-05-16 16:31:58 +0200

    feat(crud): wip

--- app/questions/mutations/createQuestion.ts
+++ app/questions/mutations/createQuestion.ts
@@ -3,6 +3,17 @@ import db from "db"
 import { CreateQuestion } from "../validations"
 
 export default resolver.pipe(resolver.zod(CreateQuestion), resolver.authorize(), async (input) => {
-  const question = await db.question.create({ data: input })
+  const question = await db.question.create({
+    data: {
+      ...input,
+      // labels: {
+      //   connect: await db.label.findMany({
+      //     where: {
+      //       id: { in: input.labels },
+      //     },
+      //   }),
+      // },
+    },
+  })
   return question
 })
--- app/questions/validations.ts
+++ app/questions/validations.ts
@@ -5,6 +5,6 @@ const text = z.string().min(10).max(200)
 export const CreateQuestion = z
   .object({
     text,
-    labels: z.array(z.string()).min(1).max(3),
+    labels: z.array(z.number()).min(1).max(3),
   })
   .nonstrict()
